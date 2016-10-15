ba62cd95de38ddc047e89a0a896e835a0a365492
Henry Qi <henryqi222@gmail.com>
2016-10-15 16:15:48 -0600

    completed instructions

---
+++ app.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $('.box').on('click', function(){
+		$(this).addClass('white');
+	})
+	$('.box').on('dblclick', function(){
+		$(this).removeClass('white');
+	})
+	$('#reset').on('click', function(){
+		$('.box').addClass('white');
+	})
+	var color = 'white';
+	var colors = 'red blue green yellow white';
+	
+	$('#red').on('click', function() {
+		color = 'red';
+	})
+	
+	$('#blue').on('click', function() {
+    color = 'blue';
+  })
+
+  $('#green').on('click', function() {
+    color = 'green';
+  })
+
+  $('#yellow').on('click', function() {
+    color = 'yellow';
+  })
+
+  $('#white').on('click', function() {
+    color = 'white';
+  })
+	
+	$('.box').on('click', function() {
+		$(this).addClass(color);
+	})
+	
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(colors);
+	})
+	
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
+	})
+	
+	
+	
+	
+	
+})
